86a6caa4f415ba4c779b6469994c7ead015e2b0c
Guillermo <guilletgn@msn.com>
2013-12-16 12:39:56 +0100

    edit Inter class

--- src/absis/Inter.java
+++ src/absis/Inter.java
@@ -1,5 +1,18 @@
 package absis;
 
 public class Inter {
+	private int v;  
+	 
+	  public Inter(){  
+	    v = 0;  
+	  }  
+	 
+	  public void set_valor(int valor){  
+	   
+	  }  
+	 
+	  public int get_valor(){  
+	    return v;  
+	  }  
 
 }
