7d99faefc22258a6ea74f04c88d80aa7316bb87f
Guillermo <guilletgn@msn.com>
2013-12-17 09:48:10 +0100

    edit inicia procesos

--- src/absis/Principal.java
+++ src/absis/Principal.java
@@ -6,7 +6,20 @@ public class Principal {
 		    Inter comptador = new Inter();
 			Mutex m = new Mutex();
 		    final int M = 2;  
-		    Proces processos[] = new Proces[M]; // dimensiona l’array de processos  
+		    Proces processos[] = new Proces[M]; 
+		    
+		    for(int i = 0; i < M; i++) {
+				processos[i] = new Proces(comptador, m, i); // crea el proces ièsim
+				//processos[i].start(); // i l'arrenca
+			}
+		 
+		    for (int i = 0; i < M; i++) {  
+		      //try{  
+		        //processos[i].join(); //Espera la finalització del proces ièsim  
+		      //}catch(InterruptedException e){}  
+		    }  
+		 
+		    System.out.println("El resultat final és = " + comptador.get_valor());   
 	 }
 	
 }
