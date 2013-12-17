fdef2a18389a3d74b860dcd382c87e79c2f107f8
Guillermo <guilletgn@msn.com>
2013-12-17 09:23:14 +0100

    edit constructor

--- src/absis/Proces.java
+++ src/absis/Proces.java
@@ -5,5 +5,12 @@ public class Proces {
 	  private Inter comptador;
 	  public int id;
 	  Mutex m;  
+	  
+	  public Proces(Inter comptador, Mutex m, int id){  
+		    this.comptador = comptador;  
+			this.m = m;
+			this.id=id;
+		  }  
+		
 
 }
