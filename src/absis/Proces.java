7332e9d973d8a998c9f5ce6c1f0cad4b66d41405
Guillermo <guilletgn@msn.com>
2013-12-17 09:26:15 +0100

    edit void run
    funciones que dependen de otras clases están comentadas

--- src/absis/Proces.java
+++ src/absis/Proces.java
@@ -12,5 +12,18 @@ public class Proces {
 			this.id=id;
 		  }  
 		
+	  public void run(){  
+	      final int N = 4;
+		  int aux=0; 
+	          for(int i = 0; i < N; i++){  
+		     // protocol d'entrada a la Zona Crítica
+	             //m.entra_zc();
+				 //aux=comptador.get_valor();
+	   			 aux++;
+	  			 //comptador.set_valor(aux);
+		    // protocol de sortida de la Zona Crítica
+	            //m.surt_zc();   
+	          }  
+	  }  
 
 }
