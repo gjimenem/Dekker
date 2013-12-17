1498a1fe98283855232b33593aaf2e4fc49a0b96
Guillermo <guilletgn@msn.com>
2013-12-17 09:45:44 +0100

    edit atrib

--- src/absis/Principal.java
+++ src/absis/Principal.java
@@ -1,5 +1,12 @@
 package absis;
 
 public class Principal {
-
+	
+	 public static void main(String[] arg){  
+		    Inter comptador = new Inter();
+			Mutex m = new Mutex();
+		    final int M = 2;  
+		    Proces processos[] = new Proces[M]; // dimensiona l’array de processos  
+	 }
+	
 }
